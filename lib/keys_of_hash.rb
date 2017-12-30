b4bacac46961caf5fd76ac344714a1e5eb391446
Katy Carr <khcarr9@gmail.com>
2017-12-30 19:39:40 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,8 @@
 class Hash
   def keys_of(instance,*arguments)
-    binding.pry
     # code goes here
     array = []
-    argumets.each do |argument|
+    arguments.each do |argument|
       if instance.keys.include?(argument)
         array.push(argument)
       end
